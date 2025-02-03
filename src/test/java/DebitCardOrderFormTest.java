adebb23b9b44b740eb7e69e8b7a0c00f67e3a2f9
Sadbarg <sadbargo4ka93@yandex.ru>
2025-02-03 13:45:16 +0300

    orderDebitCard

---
+++ src/test/java/DebitCardOrderFormTest.java
@@ -0,0 +1,60 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DebitCardOrderFormTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        // options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldHappyPathForm() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Хушвахтова Садбарг");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79012345678");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        WebElement actualElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        String actualText = actualElement.getText().trim();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        Assertions.assertTrue(actualElement.isDisplayed());
+    }
+
+    @Test
+    void shouldSendRequestEmptyField() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79012345678");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        WebElement actualElement = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input_sub"));
+        String actualText = actualElement.getText().trim();
+
+        assertEquals("Поле обязательно для заполнения", actualText);
+        Assertions.assertTrue(actualElement.isDisplayed());
+    }
+}
